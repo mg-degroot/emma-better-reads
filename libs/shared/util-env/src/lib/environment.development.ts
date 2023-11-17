09da43feb80fac10ffc2ce4006338a2aceb4195a
Emma van den Broek <emma30121998@gmail.com>
2023-11-17 12:53:17 +0100

    environment

---
+++ libs/shared/util-env/src/lib/environment.development.ts
@@ -0,0 +1,6 @@
+import { IEnvironment } from './environment.interface';
+
+export const environment: IEnvironment = {
+  production: false,
+  dataApiUrl: 'https://localhost:3000/api/user',
+};
---
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -0,0 +1,6 @@
+import { IEnvironment } from './environment.interface';
+
+export const environment: IEnvironment = {
+  production: true,
+  dataApiUrl: 'https://better-reads.azurewebsites.net',
+};
