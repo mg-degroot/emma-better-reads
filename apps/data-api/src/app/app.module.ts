b1687b895767ae59db079888c1fcd696bb132893
Emma van den Broek <emma30121998@gmail.com>
2023-11-23 17:20:33 +0100

    connectie api writer werkend

--- apps/data-api/src/app/app.module.ts
+++ apps/data-api/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BackendFeaturesUserModule } from '@nx-emma-indiv/backend/features';
 import { BackendFeaturesBookModule } from '@nx-emma-indiv/backend/features';
+import { BackendFeaturesWriterModule } from '@nx-emma-indiv/backend/features';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [BackendFeaturesUserModule, BackendFeaturesBookModule],
+  imports: [BackendFeaturesUserModule, BackendFeaturesBookModule, BackendFeaturesWriterModule],
   controllers: [AppController],
   providers: [AppService],
 })
