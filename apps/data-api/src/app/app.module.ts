7673532ad689e81c3c1a77c5fdfd85ceb2608887
Emma van den Broek <emma30121998@gmail.com>
2023-11-28 11:02:18 +0100

    writer list styling

--- apps/data-api/src/app/app.module.ts
+++ apps/data-api/src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
 import { BackendFeaturesUserModule } from '@nx-emma-indiv/backend/features';
 import { BackendFeaturesBookModule } from '@nx-emma-indiv/backend/features';
 import { BackendFeaturesWriterModule } from '@nx-emma-indiv/backend/features';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+//import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
   imports: [BackendFeaturesUserModule, BackendFeaturesBookModule, BackendFeaturesWriterModule],
