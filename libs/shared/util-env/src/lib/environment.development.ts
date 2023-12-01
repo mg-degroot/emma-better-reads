1b15a17f03b1852326f9dc88c9bcb516efbfa99b
Emma van den Broek <emma30121998@gmail.com>
2023-12-01 20:02:09 +0100

    env

--- libs/shared/util-env/src/lib/environment.development.ts
+++ libs/shared/util-env/src/lib/environment.development.ts
@@ -3,5 +3,5 @@ import { IEnvironment } from './environment.interface';
 export const environment: IEnvironment = {
   production: false,
   dataApiUrl: 'http://localhost:3000',
-  mongo: 'mongodb+srv://emma30121998:Aiki2014@cluster0.xlhirv9.mongodb.net/',
+  mongo: 'mongodb+srv://emma30121998:BRAmNA5pzY4kwjnb@cluster0.xlhirv9.mongodb.net/',
 };
--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -3,5 +3,5 @@ import { IEnvironment } from './environment.interface';
 export const environment: IEnvironment = {
   production: true,
   dataApiUrl: 'https://better-reads.azurewebsites.net',
-  mongo: 'mongodb+srv://emma30121998:Aiki2014@cluster0.xlhirv9.mongodb.net/',
+  mongo: 'mongodb+srv://emma30121998:BRAmNA5pzY4kwjnb@cluster0.xlhirv9.mongodb.net/',
 };
